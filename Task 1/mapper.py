9d4c27e172a4bb8538d38457b44ad50f34fdad04
Manasa S M <manasarow2001@gmail.com>
2021-10-13 14:16:57 +0530

    added mapper.py and reducer.py for Task 1

--- Task 1/mapper.py
+++ Task 1/mapper.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+import sys
+
+for line in sys.stdin:
+    data=line.strip().split()
+    i,j=data[0],data[1]
+    if (len(i)==1):
+        print("0"+i,j)
+    else:
+        print(i,j)
+
+
