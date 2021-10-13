bd46ad52dfac5ae8527a9aacad57fd4290daa771
Manasa S M <manasarow2001@gmail.com>
2021-10-13 14:57:51 +0530

    used zfill

--- Task 1/mapper.py
+++ Task 1/mapper.py
@@ -5,23 +5,4 @@ for line in sys.stdin:
     data=line.strip().split()
     i, j = data[0], data[1]
     #making 'i' a 10 digit number
-    if (len(i) == 1):
-        print("000000000" + i,j)
-    if (len(i) == 2):
-        print("00000000" + i,j)
-    if (len(i) == 3):
-        print("0000000" + i,j)
-    if (len(i) == 4):
-        print("000000" + i,j)
-    if (len(i) == 5):
-        print("00000" + i,j)
-    if (len(i) == 6):
-        print("0000" + i,j)
-    if (len(i) == 7):
-        print("000" + i,j)
-    if (len(i) == 8):
-        print("00" + i,j)
-    if (len(i) == 9):
-        print("0" + i,j)
-    if (len(i) == 10):
-        print(i,j)
+    print(i.zfill(10), j)
