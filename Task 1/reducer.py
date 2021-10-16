071ba8fe8e95095d2ca4fea6f156114cad89c676
Manasa S M <manasarow2001@gmail.com>
2021-10-16 20:34:31 +0530

    changed delimiter to $

--- Task 1/reducer.py
+++ Task 1/reducer.py
@@ -8,12 +8,12 @@ adj_list = []
 file1 = open(sys.argv[1], 'w')
 
 for line in sys.stdin:
-    data=line.strip().split()
-    from_node,to_node=int(data[0]),int(data[1])   
+    data = line.strip().split()
+    from_node, to_node = int(data[0]), int(data[1])   
 
     if from_node != current_from_node:
         if current_from_node != None:
-            print(current_from_node, adj_list)
+            print(current_from_node, adj_list, sep='$')
             file1.write(str(current_from_node) + ",1\n")
         adj_list = []
         current_from_node = from_node
@@ -21,6 +21,6 @@ for line in sys.stdin:
     adj_list.append(to_node)
 
 if adj_list:
-    print(current_from_node, adj_list)
+    print(current_from_node, adj_list, sep='$')
     file1.write(str(current_from_node) + ",1")
 
