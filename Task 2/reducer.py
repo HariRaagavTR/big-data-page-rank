fa7807bc7fdd6a7829c146acf5fae8e6e806c2a1
HariRaagavTR <harirams01@gmail.com>
2021-10-21 20:14:42 +0530

    Added reducer.py

--- Task 2/reducer.py
+++ Task 2/reducer.py
@@ -5,16 +5,19 @@ current_to_node = None
 total_contribution = 0
 
 for line in sys.stdin:
-    content = line.split(",")
-    to_node = int(content[0]) 
-    contribution = float(content[1]) 
+    content = line.split(',')
+    to_node = int(content[0])
+    contribution = float(content[1])
+    
     if to_node != current_to_node: 
         if current_to_node != None: 
             new_page_rank = 0.15 + 0.85 * total_contribution 
-            print(current_to_node, round(new_page_rank,2), sep=",")
+            print(current_to_node, round(new_page_rank, 2), sep = ',')
         current_to_node = to_node 
         total_contribution = 0
+    
     total_contribution += contribution 
+    
 if current_to_node != None:
-    new_page_rank = 0.15 + 0.85 * total_contribution 
-    print(current_to_node, round(new_page_rank,2), sep=",")
+    new_page_rank = 0.15 + 0.85 * total_contribution
+    print(current_to_node, round(new_page_rank, 2), sep = ',')
